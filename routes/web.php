96e141ae73b37491812ba878486e7d59ca0972d1
Abhinav <abhinavpal8960@gmail.com>
2025-12-27 20:17:06 +0530

    project initial run

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/test', function () {
+    return 'Laravel is working!';
+});
+
 Route::get('/login', function (): RedirectResponse {
     return redirect(route('filament.admin.auth.login'));
 })->name('login');
